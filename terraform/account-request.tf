82aa237702c59c09df117bc3106c24575f47c6b2
Junseong Jang <junsjang@amazon.com>
2025-11-19 13:47:50 +0900

    add account request

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -29,3 +29,35 @@ module "sandbox_child_04" {
 
   account_customizations_name = "SANDBOX"
 }
+
+module "sandbox_child_05" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "junsjang+control-tower-mgmt1-child05@amazon.com"
+    AccountName  = "Child-05"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "Sandbox"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Production (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "junsjang+control-tower-mgmt1@amazon.com"
+    SSOUserFirstName = "Junseong"
+    SSOUserLastName  = "Jang"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "junsjang+control-tower-mgmt1@amazon.com"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "John Doe"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "PRODUCTION"
+}
